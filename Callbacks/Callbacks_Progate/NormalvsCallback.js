36a8733a23a44fbf3e6d97d41ea3edde1f1fef89
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-01 15:40:44 -0700

    Declear beforehand vs declear directly in the argument

---
+++ Callbacks/Callbacks_Progate/NormalvsCallback.js
@@ -0,0 +1,42 @@
+// Normal functions
+const introduceNormal = (name) => {
+  console.log(name);
+};
+
+introduceNormal("Tomoya Waniko");
+
+//Callback functions
+const introduceCallback = (callback) => {
+  callback("Tomoay Waniko");
+};
+
+introduceCallback((name) => {
+  console.log(name);
+});
+
+///====================================//
+
+//Declaring the function beforehand
+//<SETP1> define a function
+const printKen = () => {
+  console.log("Ken the Ninja");
+};
+
+//<STEP2> recives a functinon and call the function(same)
+const call = (callback) => {
+  callback();
+};
+
+call(printKen);
+// ===================================//
+
+//Declaring a function directly in the argument
+//<STEP2> recives a functinon and call the function(same)
+const call2 = (callback) => {
+  callback();
+};
+
+//<STEP1 and 3>define a function when the function call2 is called
+call2(() => {
+  console.log("Ken the Ninja");
+});
