7be33ae599faaa1d89a2d0aed8cecaabfd448049
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-01 13:23:39 -0700

    Progate JavaScript VII 3

--- Callbacks/script.js
+++ Callbacks/script.js
@@ -2,13 +2,13 @@ const printKen = () => {
   console.log("Ken the Ninja");
 };
 
-//callback is a parameter becaues it does not have()
 const call = (callback) => {
-  console.log("Calling the callback funciton");
-  //callback() is a funciton because it has ()
+  console.log("Calling the callback funciton.");
   callback();
 };
 
 call(printKen);
 
-//JavaScript Study VII
+call(() => {
+  console.log("Master Wooly");
+});
