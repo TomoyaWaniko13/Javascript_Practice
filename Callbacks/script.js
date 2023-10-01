3948a14cf5404d0fc5e5e319e3838b3ed0d08aa0
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-01 13:43:17 -0700

    Progate JavaScript VII 4

--- Callbacks/script.js
+++ Callbacks/script.js
@@ -1,14 +1,7 @@
-const printKen = () => {
-  console.log("Ken the Ninja");
-};
-
 const call = (callback) => {
-  console.log("Calling the callback funciton.");
-  callback();
+  callback("Mom", 0);
 };
 
-call(printKen);
-
-call(() => {
-  console.log("Master Wooly");
+call((name, age) => {
+  console.log(`${name} is ${age} years old.`);
 });
