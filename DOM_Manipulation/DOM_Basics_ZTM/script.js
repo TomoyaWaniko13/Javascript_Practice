9d5530d696e868169c6cd2ddab7d2e65d3298fca
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-13 14:57:03 -0700

    Practice

--- DOM_Manipulation/DOM_Basics_ZTM/script.js
+++ DOM_Manipulation/DOM_Basics_ZTM/script.js
@@ -1,9 +1,47 @@
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
-let ul = document.querySelector("ul");
+let ul = docuemnt.querySelector("ul");
 
-button.addEventListener("click", () => {
-  var li = document.createElement("li");
+const inputLength = () => input.ariaValueMax.length;
+
+const createListElement = () => {
+  let li = document.createElement;
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
-});
+  input.value = "";
+};
+
+const addListAfterClick = () => {
+  if (inputLength() > 0) {
+    createListElement;
+  }
+};
+
+const addListAfterKeypress = (event) {
+    if(inputLength() > 0 && event.key === "Enter"){
+        createListElement();
+    }
+}
+
+
+// function inputLength() {
+//   return input.value.length;
+// }
+
+// button.addEventListener("click", () => {
+//   if (input.value.length > 0) {
+//     let li = document.createElement("li");
+//     li.appendChild(document.createTextNode(input.value));
+//     ul.appendChild(li);
+//     input.value = "";
+//   }
+// });
+
+// input.addEventListener("keydown", (event) => {
+//   if (input.value.length > 0 && event.key === "Enter") {
+//     let li = document.createElement("li");
+//     li.appendChild(document.createTextNode(input.value));
+//     ul.appendChild(li);
+//     input.value = "";
+//   }
+// });
