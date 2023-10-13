7795e03dbc0026af7ecff318b0b8ee34d11767a3
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-12 19:53:30 -0700

    Reduce()

--- AdvancedJavascript/advancedArray.js
+++ AdvancedJavascript/advancedArray.js
@@ -1,24 +1,25 @@
-var array = [1, 2, 10, 16];
+var array = [1, 2, 3, 4, 16];
 
 const double = [];
-
-//you should use map.
-const newArray = array.forEach((num) => {
-  num * 2;
-});
+const newArray = array.forEach((num) => double.push(num * 2););
 
 console.log(newArray);
 
 //map, filter, reduce
 
-//no side effects because
-//it returns a value
-const mapArray = array.map((num) => num * 2);
+const mapArray = array.map(num =>  num * 2);
 
 console.log("map", mapArray);
 
-const filerArray = array.filter((num) => num === 10);
+const filterArray = array.filter(num => num === 5);
 
-const reduceArray = arrray.reduce((accumulator, num) => {
+console.log('filter',filterArray);
+
+//reduce
+// () is parameters {} is how to handle these parameters
+const reduceArray = array.reduce((accumulator,num) =>{
   return accumulator + num;
-});
+},5);
+
+console.log('reduce',reduceArray);
+
