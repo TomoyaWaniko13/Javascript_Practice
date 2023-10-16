6320b845c1961e7364aaaea65d7c140710c0719a
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-15 20:22:54 -0700

    ES6 Practice

--- AdvancedJavascript/ES6/script.js
+++ AdvancedJavascript/ES6/script.js
@@ -16,8 +16,59 @@ if (experience > 90) {
 console.log("outer b:", b);
 
 let wizardLevel = false;
+
 if (experience > 90) {
   let wizardLevel = true;
   console.log("inside", wizardLevel);
 }
 console.log("outside", wizardLevel);
+
+const lastName = "Tomoya";
+
+const obj = {
+  name: "Tomoya",
+  age: 24,
+  isCat: false,
+};
+
+// const name = obj.name;
+// const age = obj.age;
+// let isCat = obj.isCat;
+
+const { name, age } = obj;
+let { isCat } = obj;
+
+const name2 = "Akira";
+
+const obj2 = {
+  [name2]: "hello",
+  [1 + 2]: "hihi",
+};
+
+console.log(obj2.Akira);
+
+const d = "Tomoya";
+const e = "true";
+const f = {};
+
+const obj3 = {
+  d,
+  e,
+  f,
+};
+
+// const obj3 = {
+//   d: d,
+//   e: e,
+//   f: f,
+// };
+
+const nameX = "Tomoya";
+const ageX = 100;
+const pet = "cat";
+
+const greetingBest = `Hello ${nameX} you seem to be ${
+  ageX - 10
+}. What a lovely ${pet} you have.`;
+
+console.log(greetingBest);
