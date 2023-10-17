4ef4d3565c88e375bb0129777e13e449b90f1015
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-16 19:30:25 -0700

    Advanced Funciton

--- AdvancedJavascript/AdvancedFunction/advancedFunction.js
+++ AdvancedJavascript/AdvancedFunction/advancedFunction.js
@@ -1,3 +1,14 @@
+function first() {
+  var greet = "Hi";
+  function second() {
+    alert(greet);
+  }
+  return second;
+}
+
+var newFunc = first();
+newFunc();
+
 const first = () => {
   const greet = "Hi";
   const second = () => {
@@ -8,28 +19,3 @@ const first = () => {
 
 const newFunc = first();
 newFunc();
-//Clousers = a function ran, the function excuted,
-//it is never going to excute again.
-//But it's going to remenber that there are references to those variables.
-//so the child scope always has acess to the parent scope.
-
-//Curring
-const multiply = (a, b) => (a = b);
-//a funciton in a function
-//converting a function that takes mutiple arguments
-//into a function that takes them one at a time.
-const curriedMultiply = (c) => (d) => c * d;
-let e = curriedMultiply(3)(4);
-console.log(`The value is ${e}`);
-
-//Compose
-const compose = (f, g) => (a) => f(g(a));
-const sum = (num) => num + 1;
-console.log(compose(sum, sum)(5));
-
-//Avoiding Side Effects, funcitonal purity.
-var a = 1;
-function b() {
-  // Side Effect.
-  a = 2;
-}
