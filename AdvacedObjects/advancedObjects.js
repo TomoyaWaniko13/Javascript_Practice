406b66c3913f9e66fb9ea603461fc81401fdd32f
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-17 07:15:08 -0700

    super()

---
+++ AdvacedObjects/advancedObjects.js
@@ -0,0 +1,41 @@
+//reference type
+var object1 = { value1: 10 };
+var object2 = object1;
+var object3 = { value: 10 };
+
+//context vs scope
+function a() {
+  let a = 4;
+}
+
+//instantiation
+const object4 = {
+  a: function () {
+    console.log(this);
+  },
+};
+
+class Player {
+  constructor(name, type) {
+    console.log(this);
+    this.name = name;
+    this.type = type;
+  }
+  introduce() {
+    console.log(`Hi I am ${this.name}, I am a ${this.type}`);
+  }
+}
+
+class Wizard extends Player {
+  constructor(name, type) {
+    super(name, type);
+    //super() take us up to the constructor of the Player:
+    super(name, type);
+  }
+  play() {
+    console.log(`I'm a ${this.type}`);
+  }
+}
+
+const wizard1 = new Wizard("Shelly", "Healer");
+const wizard2 = new Wizard("Shawn", "Dark Magic");
