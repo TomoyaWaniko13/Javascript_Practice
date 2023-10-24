0ee0a2a388e5fe4357bc5ceb4879bfcf87f11ca8
TomoyaWaniko <alligatorfree12@gmail.com>
2023-10-24 13:13:07 -0700

    NPM Practice

--- DOM_Manipulation/background_generator_ZTM/background.js
+++ DOM_Manipulation/background_generator_ZTM/background.js
@@ -1,3 +1,7 @@
+var _ = require("lodash");
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("answer", _.without(array, 2));
 const css = document.querySelector("h3");
 const color1 = document.getElementById("color1");
 const color2 = document.getElementById("color2");
