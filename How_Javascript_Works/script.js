7b6ea0aa09387b1d35fd7172a08459bb9d44f7ce
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-10-23 20:56:17 -0700

    asynchronous

--- How_Javascript_Works/script.js
+++ How_Javascript_Works/script.js
@@ -3,6 +3,7 @@ const b = 10;
 const c = 100;
 
 console.log("1");
+//What if this is a big task?
 console.log("2");
 console.log("3");
 
@@ -18,3 +19,20 @@ const one = () => {
 // console.log("4");
 // two()
 // one()
+
+//Recursion = a function that calls itself.
+function foo() {
+  foo();
+}
+
+console.log("1");
+setTimeout(() => {
+  console.log("2 and 2000 miliseconds");
+}, 2000);
+console.log("3");
+
+console.log("1");
+setTimeout(() => {
+  console.log("2 and 0 milisecond");
+}, 0);
+console.log("3");
