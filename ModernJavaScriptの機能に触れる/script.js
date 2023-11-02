b60dd14b1e873ce34aca837f677247a10941d597
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-02 16:11:33 -0700

    ？????

--- ModernJavaScriptの機能に触れる/script.js
+++ ModernJavaScriptの機能に触れる/script.js
@@ -1,23 +1,29 @@
-console.log("Hello");
-var val1 = "var variable";
-console.log(val1);
+// console.log("Hello");
+// var val1 = "var variable";
+// console.log(val1);
 
-//val can be rewritten
-val1 = "rewrite val1";
-console.log(val1);
+// //val can be rewritten
+// val1 = "rewrite val1";
+// console.log(val1);
 
-//val can be decleared
-var val1 = "Redclear val1";
-console.log(val1);
+// //val can be decleared
+// var val1 = "Redclear val1";
+// console.log(val1);
 
-let val2 = "let variables";
-console.log(val2);
+// let val2 = "let variables";
+// console.log(val2);
 
-//let can be rewritten
-val2 = "rewrite let";
-console.log(val2);
+// //let can be rewritten
+// val2 = "rewrite let";
+// console.log(val2);
 
-let val2 = "declear let again";
+// let val2 = "declear let again";
 
-const val3 = "const variable";
-console.log(val3);
+// const val3 = "const variable";
+// console.log(val3);
+
+// //const cannot be rewritten
+// val3 = "const variable";
+
+// //const cannot be redcleared
+// const val3 = "redclear const";
