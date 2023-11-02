cb0adb73870b9928223b1f1529d5c4f1f6c7eaaa
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-02 15:52:22 -0700

    Commit

---
+++ ModernJavaScriptの機能に触れる/script.js
@@ -0,0 +1,23 @@
+console.log("Hello");
+var val1 = "var variable";
+console.log(val1);
+
+//val can be rewritten
+val1 = "rewrite val1";
+console.log(val1);
+
+//val can be decleared
+var val1 = "Redclear val1";
+console.log(val1);
+
+let val2 = "let variables";
+console.log(val2);
+
+//let can be rewritten
+val2 = "rewrite let";
+console.log(val2);
+
+let val2 = "declear let again";
+
+const val3 = "const variable";
+console.log(val3);
