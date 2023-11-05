5970c663e9dfa6278e8ade7b4504dee247d7e652
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-05 05:01:33 -0800

    Add functionality

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -1,7 +1,18 @@
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  //reset input
+  document.getElementById("add-text").value = "";
+
+  //div tag is generated
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li tag is generated
+  const li = document.createElement("li");
+  li.innerText = inputText;
 };
 
+//when we click on the "add-button", onClick() function will run.
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
