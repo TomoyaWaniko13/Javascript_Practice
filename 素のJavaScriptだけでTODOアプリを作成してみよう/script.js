0812f55e6c9ec1b95d8193187327707d6df8f3e9
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-05 13:05:06 -0800

    k

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -16,15 +16,23 @@ const onClickAdd = () => {
   //button tag(complete) is generated.
   const completeButton = document.createElement("button");
   completeButton.innerText = "done";
+
+  //delete the parent tag(div) of the button that was clicked on
   completeButton.addEventListener("click", () => {
-    alert("Done");
+    const addTarget = completeButton.parentNode;
+
+    //get a text about TODO
+    const text = addTarget.firstElementChild.innerText;
+
+    deleteFromIncompleteList(completeButton.parentNode);
   });
 
   //button tag(imcomplete) is generated.
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "delete";
   deleteButton.addEventListener("click", () => {
-    alert("delete");
+    //delete the parent tag(div) of the button that was clicked on
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   //li tag is added to div
@@ -35,7 +43,11 @@ const onClickAdd = () => {
   document.getElementById("incomplete-list").appendChild(div);
 };
 
-//when we click on the "add-button", onClick() function run runs.
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//when we click on the "add-button", onClick() function runs.
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
