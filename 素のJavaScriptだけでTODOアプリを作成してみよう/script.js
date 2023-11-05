dff4278e53b0ef2bd310531dc67a5a3635117b7b
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-05 09:09:58 -0800

    Create add text functionality

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -1,18 +1,26 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
+  console.log(inputText);
   //reset input
   document.getElementById("add-text").value = "";
 
   //div tag is generated
   const div = document.createElement("div");
   div.className = "list-row";
+  console.log(div);
 
   //li tag is generated
   const li = document.createElement("li");
   li.innerText = inputText;
+
+  //li tag is added to div
+  div.appendChild(li);
+  console.log(div);
+
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
-//when we click on the "add-button", onClick() function will run.
+//when we click on the "add-button", onClick() function run runs.
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
