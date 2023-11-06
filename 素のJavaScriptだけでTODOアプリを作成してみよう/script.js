755591b66defffda523d5a2814d42a40e770984d
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-05 19:08:29 -0800

    Create add button functionality

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -1,18 +1,37 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+  console.log(`This is an input text: ${inputText}`);
 
   const div = document.createElement("div");
   div.className = "list-row";
   console.log(div);
 
   const li = document.createElement("li");
-  li.innerText = "list-row";
+  li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
 
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   document.getElementById("incomplete-list").appendChild(div);
+
+  completeButton.innerText = "done";
+  completeButton.addEventListener("click", () => {
+    console.log("Done button is working");
+    document.getElementById("complete-list").removeChild();
+  });
+
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    console.log(deleteTarget);
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 };
 
 document.getElementById("add-button").addEventListener("click", () => {
