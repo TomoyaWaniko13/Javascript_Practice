63b3f0cf4c9bda4b1d132a5ea76e4f448ba97d0d
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-06 08:53:56 -0800

    Add delete functionality

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -1,8 +1,8 @@
 const onClickAdd = () => {
-  const inputText = document.getElementById("add-text").value;
+  const inputText = document.getElementById("add-text").value.trim();
 
   //if the text is not empty
-  if (inputText !== null) {
+  if (inputText) {
     document.getElementById("add-text").value = "";
 
     const div = document.createElement("div");
@@ -14,8 +14,35 @@ const onClickAdd = () => {
     const completeButton = document.createElement("button");
     completeButton.innerText = "done";
 
+    completeButton.addEventListener("click", () => {
+      deelteFromIncompleteList(completeButton.parentNode);
+
+      //create a div tag
+      const addTarget = completeButton.parentNode;
+      //get a text from li tag
+      const text = addTarget.firstElementChild.innerText;
+      //reset under the div
+      addTarget.textContent = null;
+
+      //create a li tag
+      const li = document.createElement("li");
+      li.innerText = text;
+
+      const undoButton = document.createElement("button");
+      undoButton.innerText = "undo";
+
+      addTarget.appendChild(li);
+      addTarget.appendChild(undoButton);
+
+      document.getElementById("complete-list").appendChild(addTarget);
+    });
+
     const deleteButton = document.createElement("button");
+
     deleteButton.innerText = "delete";
+    deleteButton.addEventListener("click", () => {
+      deelteFromIncompleteList(deleteButton.parentNode);
+    });
 
     div.appendChild(li);
     div.appendChild(completeButton);
@@ -29,3 +56,7 @@ const onClickAdd = () => {
 document.getElementById("add-button").addEventListener("click", () => {
   onClickAdd();
 });
+
+const deelteFromIncompleteList = (parent) => {
+  document.getElementById("incomplete-list").removeChild(parent);
+};
