5e7419525ffb29fe62c3b6db1159294bcf806c1d
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-15 09:47:05 -0800

    Create add text functionality

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -1,25 +1,50 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("input-text").value.trim();
+  console.log(inputText);
 
   if (inputText) {
     document.getElementById("input-text").value = "";
 
+    const li = document.createElement("li");
+
     const div = document.createElement("div");
     div.className = "list-row";
-
-    const li = document.createElement("li");
 
     const p = document.createElement("p");
     p.innerText = inputText;
 
     li.appendChild(div);
     div.appendChild(p);
-
-    console.log(li);
     document.getElementById("unfinished-area-ul").appendChild(li);
   }
 };
 
-document
-  .getElementById("add-button")
-  .addEventListener("click", () => onClickAdd());
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickAdd();
+});
+
+// const onClickAdd = () => {
+//   const inputText = document.getElementById("input-text").value.trim();
+
+//   if (inputText) {
+//     document.getElementById("input-text").value = "";
+
+//     const div = document.createElement("div");
+//     div.className = "list-row";
+
+//     const li = document.createElement("li");
+
+//     const p = document.createElement("p");
+//     p.innerText = inputText;
+
+//     li.appendChild(div);
+//     div.appendChild(p);
+
+//     console.log(li);
+//     document.getElementById("unfinished-area-ul").appendChild(li);
+//   }
+// };
+
+// document
+//   .getElementById("add-button")
+//   .addEventListener("click", () => onClickAdd());
