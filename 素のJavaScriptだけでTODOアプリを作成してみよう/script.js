f30e09b27735b5fc66f15e5400b78b7bee45664d
TomoyaWaniko <alligatorfree12@gmail.com>
2023-11-15 11:59:09 -0800

    Add functionality to move to an finished-area

--- 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
+++ 素のJavaScriptだけでTODOアプリを作成してみよう/script.js
@@ -16,13 +16,27 @@ const onClickAdd = () => {
     const doneButton = document.createElement("button");
     doneButton.innerText = "Done";
     doneButton.addEventListener("click", () => {
-      alert("This button is working!");
+      // const moveTarget = doneButton.parentElement.parentElement;
+      // const div = doneButton.parentElement;
+      // div.removeChild(doneButton);
+      // div.removeChild(deleteButton);
+
+      //moveTarget is the li tag
+      const moveTarget = doneButton.closest("li");
+      doneButton.nextElementSibling.remove();
+      doneButton.remove();
+
+      const undoButton = document.createElement("button");
+      undoButton.innerText = "undo";
+      moveTarget.firstElementChild.appendChild(undoButton);
+      document.getElementById("finished-area-ul").appendChild(moveTarget);
     });
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
     deleteButton.addEventListener("click", () => {
-      alert("This button is working!");
+      const deleteTarget = deleteButton.closest("li");
+      document.getElementById("unfinished-area-ul").removeChild(deleteTarget);
     });
 
     li.appendChild(div);
