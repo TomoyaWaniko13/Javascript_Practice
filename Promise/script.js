20f3b33af2f21aaa09745ec4b7737c77717368e3
TomoyaWaniko <alligatorfree12@gmail.com>
2023-12-16 14:33:08 -0800

    Update

--- Promise/script.js
+++ Promise/script.js
@@ -434,18 +434,29 @@
 //         console.log(values);
 //     })
 
-const urls = [
-    "https://jsonplaceholder.typicode.com/users",
-    "https://jsonplaceholder.typicode.com/posts",
-    "https://jsonplaceholder.typicode.com/albums"
-]
+// const urls = [
+//     "https://jsonplaceholder.typicode.com/users",
+//     "https://jsonplaceholder.typicode.com/posts",
+//     "https://jsonplaceholder.typicode.com/albums"
+// ]
+//
+// Promise.all(urls.map(url => {
+//     return fetch(url).then(resp => resp.json());
+// }))
+//     .then(results => {
+//         console.log(results[0]);
+//         console.log(results[1]);
+//         console.log(results[2]);
+//     })
+//     .catch(() => console.log("error"));
+//
 
-Promise.all(urls.map(url => {
-    return fetch(url).then(resp => resp.json());
-}))
-    .then(results => {
-        console.log(results[0]);
-        console.log(results[1]);
-        console.log(results[2]);
-    })
-    .catch(() => console.log("error"));
+const animals = {
+    tiger:23,
+    lion:5,
+    monkey:2
+}
+
+const {tiger, ...rest} = animals;
+
+console.log(tiger)
